4131782406eaeb4e9cb54a0f12ca2227159401bd
Guzel <guzel.r87@mail.ru>
2017-10-04 19:46:40 +0300

    Add project files.

---
+++ ThreeOptionsReadInt/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+/*
+Вводятся три числа, считай их разными способами,
+увеличь каждое на единицу и выведи в строчку через пробел.
+ */
+
+
+namespace ThreeOptionsReadInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b;
+            bool z = int.TryParse(Console.ReadLine(), out b);
+            int c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} {1} {2}", ++a, ++b, ++c);
+            Console.ReadKey();
+        }
+    }
+}
